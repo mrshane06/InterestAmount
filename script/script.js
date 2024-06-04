5a7dc90f433971b8b6670c38ab1e89e50595bc58
mrshane06 <niekerkkeegan@007gmail.com>
2024-06-04 16:30:07 +0200

    update

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//declaring values
 let input1 =  document.querySelector('#principal')
 let input2 =  document.querySelector('#interest')
 let button =  document.querySelector('button')
@@ -5,4 +6,5 @@ let answer =  document.querySelector('#answer')
 
 button.addEventListener('click',()=>{
     answer.innerText = ('Answer: ' + (eval(`${principal.value} * ${interest.value} /100 `).toFixed(2)))
+    //multiplies the principal value and interest then divides by 100
 })
